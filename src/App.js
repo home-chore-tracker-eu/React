855866f9d0ccdbcb69556500f72d6e58ed8aad4e
Francis Bulus <notchera@gmail.com>
2019-10-21 16:14:37 +0100

    redux store is now wired

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useSelector } from "react-redux";
 import './App.css';
 
 function App() {
+  //testing the store
+  console.log(useSelector(state => state.families))
+
   return (
     <div className="App">
       
