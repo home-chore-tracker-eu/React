1dfa817fb3b719be9bdb03ee9af2231c21350926
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-10-23 16:06:47 +0100

    alert with redirect to login - lines 29 & 30

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "./App.less";
 import MineChore from './components/MineChore'
 import Login from "./components/Login";
 import "./App.less";
-import SignUp from './components/SignUp';
 
 function App() {
   return (
@@ -13,10 +12,6 @@ function App() {
         <Route
           exact path='/'
           component={Login}
-          />
-          <Route
-          exact path='/signup'
-          component={SignUp}
           />
           <PrivateRoute path='/dashboard' component={MineChore} />
     </>
