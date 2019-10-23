a5c931043d0835e84f5ea49c1eaf2dd201c116d2
Jayne Carmichael Norrie <39460834+jaynecn@users.noreply.github.com>
2019-10-23 09:00:50 +0100

    SignUp.jsx update

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { Form, Icon, Input, Button, Checkbox } from "antd";
+import axios from 'axios';
+
+const SignUpForm = props => {
+  const { getFieldDecorator, validateFields } = props.form;
+  const handleSubmit = e => {
+    e.preventDefault();
+    validateFields((err, values) => {
+      if (!err) {
+        console.log("Here are the form values", values);
+      }
+      axios.post('https://home-chore-tracker.herokuapp.com/api/auth/register', {
+        "name": values.name, "email": values.username, "password": values.password})
+        .then(res => {
+          localStorage.setItem('token', res.data.token)
+          console.log(res.data.token)
+          })
+        .catch(err => {
+          console.log(err.message);
+          });
+    });
+  };
+  return (
+    <Form onSubmit={handleSubmit} className="login-form">
+      <Form.Item>
+        {getFieldDecorator("name", {
+          rules: [{ required: true, message: "Please input your name!" }]
+        })(
+          <Input
+            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+            placeholder="Name"
+          />
+        )}
+      </Form.Item>
+      <Form.Item>
+        {getFieldDecorator("email", {
+          rules: [{ type: 'email', required: true, message: "Please input a valid email." }]
+        })(
+          <Input
+            prefix={<Icon type="mail" style={{ color: "rgba(0,0,0,.25)" }} />}
+            placeholder="Email"
+          />
+        )}
+      </Form.Item>
+      <Form.Item>
+        {getFieldDecorator("password", {
+          rules: [{ required: true, message: "Please input your password!" }]
+        })(
+          <Input
+            prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+            type="password"
+            placeholder="Password"
+          />
+        )}
+      </Form.Item>
+      <Form.Item>
+        {getFieldDecorator("remember", {
+          valuePropName: "checked",
+          initialValue: true
+        })(<Checkbox>Remember me</Checkbox>)}
+        <Button type="primary" htmlType="submit" className="signup-form-button">
+          Sign Up
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+const SignUp = Form.create({ name: "normal_login" })(SignUpForm);
+export default SignUp;
