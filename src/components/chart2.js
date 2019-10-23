4da63236337c0603648d33cad94537e21db03082
Francis Bulus <notchera@gmail.com>
2019-10-23 19:05:16 +0100

    incorporated charts with g2m

---
+++ src/components/chart2.js
@@ -0,0 +1,18 @@
+export const chartData = [
+    {"name": 'Kevin',"tem": 10,"family": "Francis"},
+    {"name": 'John',"tem": 22,"family": "Francis"},
+    {"name": 'Terry',"tem": 20,"family": "Francis"},
+    {"name": 'Mirabel',"tem": 26,"family": "Francis"},
+    {"name": 'Kevin',"tem": 20,"family": "Francis"},
+    {"name": 'John',"tem": 26,"family": "Francis"},
+   
+  ];
+  
+  export const pieData = [{pointer: 'Overall',value: 85, length:2,y:1.05}];
+  
+  export const barData = [
+    {"name":"John","tem":9.5,"family":"Francis"},
+    {"name":"Terry","tem":44.5,"family":"Francis"},
+    {"name":"Mirabel","tem":18.2,"family":"Francis"},
+    {"name":"Kevin","tem":6.9,"family":"Francis"},
+  ];
